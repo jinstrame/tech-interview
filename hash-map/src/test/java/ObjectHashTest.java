feae8d1daa6200a81a3fbbbb80eb0c669414cb13
Evgenii Baibakov <evgenii_baibakov@epam.com>
2017-03-06 01:17:48 +0300

    Hash task test fixed

--- hash-map/src/test/java/ObjectHashTest.java
+++ hash-map/src/test/java/ObjectHashTest.java
@@ -1,4 +1,5 @@
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -12,12 +13,29 @@ public class ObjectHashTest {
         Short ss = -8;
         Byte b = 127;
         Byte bb = -128;
-        assertThat(i.hashCode(), is(6));
-        assertThat(ii.hashCode(), is(-6));
-        assertThat(s.hashCode(), is(8));
-        assertThat(ss.hashCode(), is(-8));
-        assertThat(b.hashCode(), is(127));
-        assertThat(bb.hashCode(), is(-128));
+
+        assertThat(i.toString(), is(Integer.toString(i.hashCode())));
+        assertThat(ii.toString(),is(Integer.toString(ii.hashCode())));
+        assertThat(s.toString(), is(Integer.toString(s.hashCode())));
+        assertThat(ss.toString(),is(Integer.toString(ss.hashCode())));
+        assertThat(b.toString(), is(Integer.toString(b.hashCode())));
+        assertThat(bb.toString(), is(Integer.toString(bb.hashCode())));
+
+        assertThat(i.toString(), not(i.hashCode()));
+        assertThat(ii.toString(),not(i.hashCode()));
+        assertThat(s.toString(), not(i.hashCode()));
+        assertThat(ss.toString(),not(i.hashCode()));
+        assertThat(b.toString(), not(i.hashCode()));
+        assertThat(bb.toString(), not(i.hashCode()));
+
+        assertFalse(i.toString() == new String(Integer.toString(i.hashCode())));
+        assertFalse(ii.toString() == new String(Integer.toString(ii.hashCode())));
+        assertFalse(s.toString() == new String(Integer.toString(s.hashCode())));
+        assertFalse(ss.toString() == new String(Integer.toString(ss.hashCode())));
+        assertFalse(b.toString() == new String(Integer.toString(b.hashCode())));
+        assertFalse(bb.toString() == new String(Integer.toString(bb.hashCode())));
+
+
     }
 
 }
